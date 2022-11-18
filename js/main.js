90533bae3d201e96dd767fa0a45ad70e4cbb2dbf
ChrisKun <43345604+ChrisKunN@users.noreply.github.com>
2022-11-18 10:02:53 -0300

    resubida porfolio

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*=======Active Link ========*/ 
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function activeLink(){
+    navLink.forEach((a) => a.classList.remove('active-link'));
+    this.classList.add('active-link');
+}
+
+navLink.forEach((a) => a.addEventListener('click', activeLink));
+
+
+
+
+/*=======Mixitup Filter ========*/ 
+
+let mixerProjects = mixitup('.projects__container', {
+    selectors: {
+        target: '.project__item'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Active Work */
+
+const linkWork = document.querySelectorAll('.category__btn');
+
+function activeWork(){
+    linkWork.forEach((a) => a.classList.remove('active-work'));
+    this.classList.add('active-work');
+}
+
+linkWork.forEach((a) => a.addEventListener('click', activeWork));
+
+/*=======Testimonials Swiper ========*/ 
+
+var TestiSwiper = new Swiper(".testimonial__container", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+  
+
+
+
